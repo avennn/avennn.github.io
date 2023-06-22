a80c47b5902c7e234c70e76af350f98585af1784
javenleung <914301050@qq.com>
2023-06-22 22:47:33 +0800

    feat: markdown lint, post categories and tags

---
+++ .markdownlint-cli2.mjs
@@ -0,0 +1,94 @@
+// fields: categories + tags
+const restrictBlogFields = {
+  names: ['restrict-blog-fields'],
+  description: 'Blog fields',
+  tags: ['test'],
+  function: (params, onError) => {
+    const { frontMatterLines, config = {} } = params;
+    const { categories: allowedCategories = [], tags: allowedTags = [] } = config;
+    if (frontMatterLines) {
+      const categoryLine = frontMatterLines.find((item) => /categories:\s*\[.*\]/.test(item));
+      const tagLine = frontMatterLines.find((item) => /tags:\s*\[.*\]/.test(item));
+
+      if (!categoryLine) {
+        onError({
+          lineNumber: 1,
+          detail: 'Should have "categories" field',
+        });
+      }
+      if (!tagLine) {
+        onError({
+          lineNumber: 1,
+          detail: 'Should have "tags" field',
+        });
+      }
+
+      const matchedCategories = categoryLine.match(/categories:\s*\[(.*)\]/);
+      const matchedTags = tagLine.match(/tags:\s*\[(.*)\]/);
+
+      if (matchedCategories) {
+        const categories = matchedCategories[1]
+          .split(',')
+          .map((item) => item.trim())
+          .filter(Boolean);
+        if (
+          !categories.length ||
+          categories.length > 1 ||
+          !allowedCategories.includes(categories[0])
+        ) {
+          onError({
+            lineNumber: 1,
+            detail: `Categories should be one of [${allowedCategories.join(', ')}]`,
+          });
+        }
+      }
+      if (matchedTags) {
+        const tags = matchedTags[1]
+          .split(',')
+          .map((item) => item.trim())
+          .filter(Boolean);
+        if (!tags.length || tags.some((tag) => !allowedTags.includes(tag))) {
+          onError({
+            lineNumber: 1,
+            detail: `Tags should be one or more of [${allowedTags.join(', ')}]`,
+          });
+        }
+      }
+    }
+  },
+};
+
+let no = 1;
+const allBuiltInRules = {};
+while (no < 54) {
+  allBuiltInRules[no > 9 ? `MD0${no}` : `MD00${no}`] = false;
+  no++;
+}
+
+export default {
+  config: {
+    default: true,
+    ...allBuiltInRules,
+    'restrict-blog-fields': {
+      // Define categories and tags here.
+      categories: ['前端', '后端', 'Linux', '架构', 'AI', '理财', '职场', '生活随笔'],
+      tags: [
+        'js',
+        'css',
+        'html',
+        'ts',
+        'nodejs',
+        'react',
+        'vue',
+        '浏览器',
+        '微前端',
+        'web安全',
+        'shell',
+        '工具',
+        '硬链接',
+        '软链接',
+      ],
+    },
+  },
+  customRules: [restrictBlogFields],
+};
--- README.md
+++ README.md
@@ -4,15 +4,8 @@
 
 https://realfavicongenerator.net/
 
-## 规范
-
-category 只允许 1 个，为'前端'|'后端'| 'Linux' | '架构'|'职场'|'AI'|'理财'|'生活随笔'等大领域。
-
-tag 允许很多，包括 js，css，html，浏览器，react，vue，微前端，nodejs 等。
-
 ## TODO
 
-- [ ] markdown lint 或者 commit lint 限制 category 和 tag
 - [ ] 图片压缩，尤其是头图
 - [ ] 头图宽高适应，自动裁剪，参考 eslint max-len
 - [ ] 字体样式加载后页面抖动问题
