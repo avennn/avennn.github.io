30234aa948ad02530e2c4132cf9b47668594aa1e
javenleung <914301050@qq.com>
2023-06-22 17:49:45 +0800

    feat: prettier format blogs.json before write in

--- tools/digest.mjs
+++ tools/digest.mjs
@@ -12,6 +12,7 @@ import fetch from 'node-fetch';
 import remarkFrontmatter from 'remark-frontmatter';
 import inquirer from 'inquirer';
 import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
+import prettier from 'prettier';
 
 dayjs.extend(timezone);
 dayjs.extend(advancedFormat);
@@ -152,4 +153,10 @@ Object.assign(manifest, {
     images,
   },
 });
-fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2));
+const prettierOpts = await prettier.resolveConfig(
+  path.join(getDirName(import.meta.url), '../.prettierrc')
+);
+fs.writeFile(
+  manifestPath,
+  prettier.format(JSON.stringify(manifest), { ...prettierOpts, parser: 'json' })
+);
